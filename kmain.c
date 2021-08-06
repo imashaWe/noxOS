a6667334ed6082b560fc86694a64bddf1e58ccf5
imashaWe <imasha98.we@gmail.com>
2021-08-06 19:39:07 +0530

    fix buggs

--- kmain.c
+++ kmain.c
@@ -6,10 +6,10 @@ int kmain()
 	char heading[] = "Welcome to NOX";
 	unsigned int len = sizeof(heading) / sizeof(heading[0]);
 	
-	//frame builder writing
+	// frame builder writing
     	fb_write(0, heading, len);
     
-    	//serial writing
+    	// serial writing
     	serial_write(0x3F8, heading, len);
     	
 	return 0;
