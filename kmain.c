df393c6cec6b725ae0aa7f34cab0fc72d820230c
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-08-13 16:49:10 +0530

    intergrate segmentation (#5)

--- kmain.c
+++ kmain.c
@@ -1,10 +1,14 @@
 #include "drivers/frame_builder.h"
 #include "drivers/serial_write.h"
+#include "memory_segmnets.h"
 
 int kmain()
 {
 	char heading[] = "Welcome to NOX";
 	unsigned int len = sizeof(heading) / sizeof(heading[0]);
+	
+	// install segments
+	segments_install_gdt();
 	
 	// frame builder writing
     	fb_write(0, heading, len);
