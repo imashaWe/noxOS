966b57e13f600c11757f74828bf2b2f66f6e4016
imashaWe <imasha98.we@gmail.com>
2021-08-20 17:15:52 +0530

    add interrupts

--- io.h
+++ io.h
@@ -2,12 +2,13 @@
 #define INCLUDE_IO_H
 
 /** outb:
-*  Sends the given data to the given I/O port. Defined in io.s
-*
-*  @param port The I/O port to send the data to
-*  @param data The data to send to the I/O port
-*/
+ *  Sends the given data to the given I/O port. Defined in io.s
+ *
+ *  @param port The I/O port to send the data to
+ *  @param data The data to send to the I/O port
+ */
 void outb(unsigned short port, unsigned char data);
+
 
 /** inb:
  *  Read a byte from an I/O port.
