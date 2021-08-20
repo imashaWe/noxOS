d93f2246937b88754a32bf6a6cba322b4c64d239
Imasha Weerakoon <67216030+imashaWe@users.noreply.github.com>
2021-08-20 18:07:40 +0530

    add interrupts (#6)

--- kmain.c
+++ kmain.c
@@ -1,20 +1,27 @@
-#include "drivers/frame_builder.h"
-#include "drivers/serial_write.h"
-#include "memory_segmnets.h"
+#include "framebuffer.h"
+#include "serial.h"
+#include "memory_segments.h"
+#include "interrupts.h"
+#include "keyboard.h"
 
 int kmain()
 {
-	char heading[] = "Welcome to NOX";
-	unsigned int len = sizeof(heading) / sizeof(heading[0]);
+	char title[] = "Welcome to NOX";
+	
+    	// move cursor
+    	fb_move_cursor(6*80);
+    	
+    	// frame builder writing
+	fb_write_str(title,sizeof(title));
+	
+	// serial writing
+	serial_write(title,sizeof(title));
 	
 	// install segments
 	segments_install_gdt();
 	
-	// frame builder writing
-    	fb_write(0, heading, len);
-    
-    	// serial writing
-    	serial_write(0x3F8, heading, len);
+	// install interrupt handler
+	interrupts_install_idt();
     	
 	return 0;
 }
